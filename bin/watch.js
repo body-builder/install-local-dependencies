939ed08711ae89e49f8f2badf7d447c2f7114e1f
bencergazda <benedek.kaldi@srg.hu>
2021-01-15 12:47:04 +0100

    fix: `ignored_packages` config option also for watch (fixes f8b02703)

--- bin/watch.js
+++ bin/watch.js
@@ -1,11 +1,13 @@
+#!/usr/bin/env node
+
 const getConfig = require('../src/config');
 const { prepare_dependencies, watch_dependencies } = require('../src/project');
 const { delete_tarball } = require('../src/dependency');
 
 async function watch() {
-	const { cwd, temp_path, modules_path, manager, types, ignore_list } = await getConfig();
+	const { cwd, temp_path, modules_path, manager, types, ignore_list, ignored_packages } = await getConfig();
 
-	const { mocked_dependencies, packed_dependencies } = await prepare_dependencies({ types, cwd, temp_path, ignore_list });
+	const { mocked_dependencies, packed_dependencies } = await prepare_dependencies({ types, cwd, temp_path, ignore_list, ignored_packages });
 
 	// Delete tarballs
 	await Promise.all(packed_dependencies.map(({ tarball_name }) => delete_tarball(tarball_name, { temp_path })));
